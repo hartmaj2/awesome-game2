34a793533dfe7340244c7133d5a36945cc463591
SADOVSKV <3217cody@gmail.com>
2024-11-23 18:19:26 +0100

    player movement updated

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,12 +2,13 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 var dir = Vector2.ZERO
-
+var moving = false
+var shooting = false
 
 func _physics_process(delta: float) -> void:
 	dir = Vector2(Input.get_action_strength("right")-Input.get_action_strength("left"),
 	Input.get_action_strength("down")-Input.get_action_strength("up"))
-
+	shooting = Input.is_action_just_pressed("shoot")
+	var zero = Vector2.ZERO
 	move_and_slide()
