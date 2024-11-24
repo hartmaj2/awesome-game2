67d06c3ae86cda4d192e7ada9d8313295a27c9a0
SADOVSKV <3217cody@gmail.com>
2024-11-24 16:55:28 +0100

    sound while moving fixed

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,17 +5,25 @@ const SPEED = 300.0
 var dir = Vector2.ZERO
 var moving = false
 var shooting = false
+var sound_end = false
 
 func _physics_process(delta: float) -> void:
 	dir = Vector2(Input.get_action_strength("right")-Input.get_action_strength("left"),
 	Input.get_action_strength("down")-Input.get_action_strength("up"))
 	shooting = Input.is_action_just_pressed("shoot")
-	var zero = Vector2.ZERO
+	var zero = (Vector2.ZERO == dir)
 	if (zero and moving):
 		moving = false
 		$PlayerSounds.stop()
 	elif (!zero and !moving):
 		moving=true
 		$PlayerSounds.play()
+	elif (!zero and sound_end):
+		$PlayerSounds.play()
+		sound_end = false
 	position += SPEED*delta*dir
 	move_and_slide()
+
+
+func _on_player_sounds_finished() -> void:
+	sound_end = true
