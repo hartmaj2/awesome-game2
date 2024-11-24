f738c37ffb6f182d85cc7c808b7cb83081e57d31
SADOVSKV <3217cody@gmail.com>
2024-11-24 20:32:32 +0100

    menu works

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,6 +1,1 @@
 extends Node2D
-
-func _process(delta: float) -> void:
-	if (Input.is_action_just_pressed("settings")):
-		print("esc")
-		$Settings.visible = true
--- scripts/settings.gd
+++ scripts/settings.gd
@@ -1,7 +1,17 @@
-extends Node2D
+extends Control
 var prev = null
 
 
+#func _ready() -> void:
+	#visible = false
+	 
+func _input(event : InputEvent):
+	if (event.is_action_pressed("settings")):
+		visible = !visible
+		get_tree().paused = !get_tree().paused
 
-func _on_back_pressed() -> void:
+
+
+func _on_button_pressed() -> void:
 	visible = false
+	get_tree().paused = false
